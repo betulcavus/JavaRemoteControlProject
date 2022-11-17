565106f2a0b466ab1687d40d006cf297a7be2913
Betul Cavus <btlcvss@gmail.com>
2022-11-17 10:56:52 +0300

    ikinci

--- src/main/java/remotecontrol/RemoteControlRunner.java
+++ src/main/java/remotecontrol/RemoteControlRunner.java
@@ -5,7 +5,7 @@ public class RemoteControlRunner {
     public static void main(String[] args) {
 
         Test tv = new Test(102.10,80.00,40.45);
-        tv.gucDugmesi();//kumandayla tv'yi actim
+        tv.gucDugmesi();//kumandayla tv'yi ac
         System.out.println(tv.kanalDegistirme(2));//45.23
         System.out.println(tv.sesAzaltma());//11
         System.out.println(tv.sesArtirma());//12
